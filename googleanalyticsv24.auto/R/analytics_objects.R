52d690c91495e0952a5ccc76e7b19c3ac2ae20c0
Mark Edmondson <github@markedmondson.me>
2016-09-04 00:04:33 +0200

    add auto-generated run 1

---
+++ googleanalyticsv24.auto/R/analytics_objects.R
@@ -0,0 +1,11 @@
+#' Google Analytics API Objects 
+#' Views and manages your Google Analytics data.
+#' 
+#' Auto-generated code by googleAuthR::gar_create_api_objects
+#'  at 2016-09-03 22:34:00
+#' filename: /Users/mark/dev/R/autoGoogleAPI/googleanalyticsv24.auto/R/analytics_objects.R
+#' api_json: api_json
+#' 
+#' Objects for use by the functions created by googleAuthR::gar_create_api_skeleton
+
+
---
+++ googlefreebasev1.auto/R/freebase_functions.R
@@ -0,0 +1,21 @@
+#' Freebase Search
+#' Find Freebase entities using textual queries and other constraints.
+#' 
+#' Auto-generated code by googleAuthR::gar_create_api_skeleton
+#'  at 2016-09-03 23:13:01
+#' filename: /Users/mark/dev/R/autoGoogleAPI/googlefreebasev1.auto/R/freebase_functions.R
+#' api_json: api_json
+#' 
+#' @details 
+#' Authentication scopes used are:
+#' \itemize{
+#'   \item 
+#' }
+#' 
+#' @docType package 
+#' @name freebase_googleAuthR
+#' 
+NULL
+## NULL
+
+
